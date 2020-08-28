57c93127289d7a4b042e967f0866ed89ec81d96b
Lee-Sungje <dtw2813@gmail.com>
2020-08-28 21:44:23 +0900

    프로그래머스 코딩테스트 연습 문제 Level1
    ·Collatz - 콜라츠 추측
    ·DescendingOrder - 정수 내림차순으로 배치하기
    ·GCDLCM - 최대공약수와 최소공배수
    ·MakeInvertedArray - 자연수 뒤집어 배열로 만들기

---
+++ Level1/Collatz.cpp
@@ -0,0 +1,19 @@
+﻿/* 프로그래머스 - 콜라츠 추측 */
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+	int count;
+	long long n = num;
+
+	for (count = 0; count < 500; count++) {
+		if (n == 1)
+			return count;
+		n = n & 1 ? n * 3 + 1 : n / 2;
+	}
+
+	return -1;
+}
---
+++ Level1/MakeInvertedArray.cpp
@@ -0,0 +1,19 @@
+﻿/* 프로그래머스 - 자연수 뒤집어 배열로 만들기 */
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(long long n) {
+	vector<int> answer;
+	string str = to_string(n);
+
+	//for (int i = 0; i < str.size(); i++)
+	//	answer.insert(answer.begin(), str[i] - '0');
+	
+	for (int i = str.size() - 1; i >= 0; i--)
+		answer.push_back(str[i] - '0');
+
+	return answer;
+}
