6a6d3f03d05b5815d6017fef51adba7a98c78034
Lee-Sungje <dtw2813@gmail.com>
2020-08-30 21:54:09 +0900

    프로그래머스 코딩테스트 연습 문제 Level1
    ·ClawMachineGame - [2019 카카오 개발자 겨울 인턴십]크레인 인형뽑기 게임
    ·Interval - x만큼 간격이 있는 n개의 숫자
    ·MatrixAddition - 행렬의 덧셈

---
+++ Level1/ClawMachineGame.cpp
@@ -0,0 +1,31 @@
+﻿/* 프로그래머스 - [2019 카카오 개발자 겨울 인턴십]크레인 인형뽑기 게임 */
+
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int solution(vector<vector<int>> board, vector<int> moves) {
+	int answer = 0;
+	stack<int> basket;
+	
+	for (int idx1 = 0; idx1 < moves.size(); idx1++) {
+		for (int idx2 = 0; idx2 < board.size(); idx2++) {
+			int *current = &board[idx2][moves[idx1] - 1];
+			if (*current) {
+				if (!basket.empty() && basket.top() == *current) {
+					answer += 2;
+					basket.pop();
+				} else {
+					basket.push(*current);
+				}
+				*current = 0;
+
+				break;
+			}
+		}
+	}
+
+	return answer;
+}
---
+++ Level1/Interval.cpp
@@ -0,0 +1,14 @@
+﻿/* 프로그래머스 - x만큼 간격이 있는 n개의 숫자 */
+
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+	vector<long long> answer;
+
+	for (int interval = 1; interval <= n; interval++)
+		answer.push_back(x * interval);
+
+	return answer;
+}
