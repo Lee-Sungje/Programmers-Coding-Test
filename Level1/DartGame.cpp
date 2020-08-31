66dc49786d323200c688de02527587bb7adb90b0
Lee-Sungje <dtw2813@gmail.com>
2020-08-31 22:37:45 +0900

    프로그래머스 코딩테스트 연습 문제 Level1
    ·DartGame - [1차] 다트 게임
    ·PrimeNumber - 소수 찾기
    ·PrintRectangle - 직사각형 별찍기

---
+++ Level1/DartGame.cpp
@@ -0,0 +1,49 @@
+﻿/* 프로그래머스 - [1차] 다트 게임 */
+
+#include <string>
+#include <regex>
+#include <vector>
+
+#define SINGLE(x) (x)
+#define DOUBLE(x) ((x)*(x))
+#define TRIPLE(x) ((x)*(x)*(x))
+
+using namespace std;
+
+int ScoreCalculate(int score, string bonus, string option) {
+	score = bonus == "S" ? SINGLE(score) : bonus == "D" ? DOUBLE(score) : TRIPLE(score);
+
+	int multiple = 1;
+
+	if (option == "*")
+		multiple = 2;
+	else if (option == "#")
+		multiple = -1;
+
+	return score * multiple;
+}
+
+int solution(string dartResult) {
+	const int kScoreIndex = 1;
+	const int kBonusIndex = 2;
+	const int kOptionIndex = 3;
+	const int chance = 3;
+	
+	int total = 0;
+	vector<int> score(chance);
+	regex pattern("(\\d{1,2})([SDT])([*#]?)");
+	smatch match;
+
+	for (size_t index = 0; index < chance; index++) {
+		regex_search(dartResult, match, pattern);
+		
+		score[index] = ScoreCalculate(stoi(match[kScoreIndex].str()), match[kBonusIndex].str(), match[kOptionIndex].str());
+		if (!(index - 1 < 0) && match[kOptionIndex].str() == "*")
+			score[index - 1] *= 2;
+		dartResult = match.suffix();
+	}
+
+	for_each(score.begin(), score.end(), [&total](int score) { total += score; });
+
+	return total;
+}
---
+++ Level1/PrimeNumber.cpp
@@ -0,0 +1,18 @@
+﻿/* 프로그래머스 - 소수 찾기 */
+
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n) {
+	vector<bool> prime(n + 1);
+
+	prime[0] = prime[1] = true;
+	for (int i = 2; i * i <= n; i++)
+		if (!prime[i])
+			for (int j = i * i; j <= n; j += i)
+				prime[j] = true;
+	
+	return count(prime.begin(), prime.end(), false);
+}
