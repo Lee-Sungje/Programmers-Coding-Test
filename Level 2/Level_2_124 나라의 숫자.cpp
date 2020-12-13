fdc4d4ecfe6a4b5cb7fa4dcbdcf8475e70182fb6
Lee-Sungje <dtw2813@gmail.com>
2020-12-14 00:03:39 +0900

    프로그래머스 코딩테스트 연습 문제 Level2
    ·Level_2_124 나라의 숫자 - https://programmers.co.kr/learn/courses/30/lessons/12899

---
+++ Level 2/Level_2_124 나라의 숫자.cpp
@@ -0,0 +1,24 @@
+﻿/* 프로그래머스 - Level 2 124 나라의 숫자 */
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+constexpr int ZERO = 0;
+constexpr int ONE = 1;
+constexpr int NOTATION = 3;
+constexpr char number[NOTATION] = { '4', '1', '2' };
+
+string solution(int n) {
+	string answer = "";
+
+	while (n != ZERO) {
+		answer.insert(ZERO, ONE, number[n % NOTATION]);
+		if (n % NOTATION == ZERO)
+			n = n / NOTATION - ONE;
+		else
+			n = n / NOTATION;
+	}
+	return answer;
+}
