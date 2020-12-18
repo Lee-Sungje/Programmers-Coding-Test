6370c0a6424a7ef7df7a8105eb4592f0b9e8957c
Lee-Sungje <dtw2813@gmail.com>
2020-12-18 21:02:14 +0900

    프로그래머스 코딩테스트 연습 문제 Level2
    ·Level_2_프린터 - https://programmers.co.kr/learn/courses/30/lessons/42587

---
+++ Level 2/Level_2_프린터.cpp
@@ -0,0 +1,42 @@
+﻿/* 프로그래머스 - Level 2 프린터 */
+
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+typedef pair<int, int> P;
+
+queue<P> load(vector<int> priorities) {
+	queue<P> spool;
+
+	for (int location = 0; location < priorities.size(); location++)
+		spool.push({ location, priorities[location] });
+
+	return spool;
+}
+
+int solution(vector<int> priorities, int location) {
+	queue<P> spool;
+	spool = load(priorities);
+	sort(priorities.begin(), priorities.end());
+
+	int answer = 0;
+	while (!spool.empty()) {
+		P document = spool.front();
+		spool.pop();
+
+		if (document.second == priorities.back()) {
+			answer += 1;
+			priorities.pop_back();
+			if (document.first == location)
+				break;
+		} else {
+			spool.push(document);
+		}
+	}
+
+	return answer;
+}
