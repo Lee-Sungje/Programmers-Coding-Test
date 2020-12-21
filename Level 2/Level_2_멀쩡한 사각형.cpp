255ab5ad7d65af938198e218ad402990dd602b67
Lee-Sungje <dtw2813@gmail.com>
2020-12-21 19:47:19 +0900

    프로그래머스 코딩테스트 연습 문제 Level2
    ·Level_2_멀쩡한 사각형 - https://programmers.co.kr/learn/courses/30/lessons/62048

---
+++ Level 2/Level_2_멀쩡한 사각형.cpp
@@ -0,0 +1,20 @@
+﻿/* 프로그래머스 - Level 2 멀쩡한 사각형 */
+
+using namespace std;
+
+int gcd(long long a, long long b) {
+	return (a % b == 0) ? b : gcd(b, a % b);
+}
+
+long long solution(int w, int h) {
+	long long lw = w, lh = h;
+	long long answer = lw * lh - (lw + lh - gcd(lw, lh));
+
+	return answer;
+}
+
+int main() {
+	solution(8, 12);
+
+	return 0;
+}
