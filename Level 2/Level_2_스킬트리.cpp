f1a7c2bc7bd9fe9232a23c3406d117bdc019fc28
Lee-Sungje <dtw2813@gmail.com>
2020-12-24 22:29:49 +0900

    프로그래머스 코딩테스트 연습 문제 Level2
    ·Level_2_스킬트리 - https://programmers.co.kr/learn/courses/30/lessons/49993

---
+++ Level 2/Level_2_스킬트리.cpp
@@ -0,0 +1,36 @@
+﻿/* 프로그래머스 - Level 2 스킬트리 */
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+constexpr int MINUS_ONE = -1;
+constexpr int SKILL_LENGTH = 26;
+
+int solution(string skill, vector<string> skill_trees) {
+	vector<int> skill_order(SKILL_LENGTH, MINUS_ONE);
+	for (int order = 0; order < skill.length(); order++)
+		skill_order[skill[order] - 'A'] = order;
+
+	int answer = 0;
+	for (string skill_tree : skill_trees) {
+		bool isRight = true;
+		int order = 0;
+
+		for (int i = 0; i < skill_tree.length() && isRight; i++) {
+			char currnet_skill = skill_tree[i];
+
+			if (skill_order[currnet_skill - 'A'] == MINUS_ONE)
+				continue;
+			else if (skill_order[currnet_skill - 'A'] == order)
+				order += 1;
+			else
+				isRight = false;
+		}
+
+		answer += isRight ? 1 : 0;
+	}
+
+	return answer;
+}
