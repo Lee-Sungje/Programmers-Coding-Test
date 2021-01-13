31e3e5c4734b39900f3c9388933feccbacf798ed
Lee-Sungje <dtw2813@gmail.com>
2021-01-13 23:19:27 +0900

    프로그래머스 코딩테스트 연습 문제 Level2
    ·Level_2_스킬트리 - https://programmers.co.kr/learn/courses/30/lessons/42584

---
+++ Level 2/Level_2_주식가격.cpp
@@ -0,0 +1,36 @@
+﻿/* 프로그래머스 - Level 2 주식가격 */
+
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+struct Stock {
+	int timing;
+	int price;
+
+	Stock(int timing, int price) : timing(timing), price(price) {}
+};
+
+vector<int> solution(vector<int> prices) {
+	vector<int> answer(prices.size());
+	stack<Stock> stack;
+	
+	for (int time = 0; time < prices.size(); time++) {
+		while (!stack.empty() && prices[time] < stack.top().price) {
+			Stock stock = stack.top();
+			stack.pop();
+			answer[stock.timing] = time - stock.timing;
+		}
+		stack.push({ time, prices[time] });
+	}
+
+	int length = prices.size() - 1;
+	while (!stack.empty()) {
+		Stock stock = stack.top();
+		stack.pop();
+		answer[stock.timing] = length - stock.timing;
+	}
+
+	return answer;
+}
