a1c0cfea732f02a2a1d0eea0a603a6440a23e09e
Lee-Sungje <dtw2813@gmail.com>
2021-01-15 23:52:52 +0900

    프로그래머스 코딩테스트 연습 문제 Level2
    ·Level_2_소수찾기 - https://programmers.co.kr/learn/courses/30/lessons/42839

---
+++ Level 2/Level_2_소수찾기.cpp
@@ -0,0 +1,46 @@
+﻿/* 프로그래머스 - Level 2 124 나라의 숫자 */
+
+#include <algorithm>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<bool> primeNumbers;
+
+void eratos(int n) {
+	if (n <= 1) return;
+
+	primeNumbers = vector<bool>(n + 1, true);
+	primeNumbers[0] = primeNumbers[1] = false;
+	for (int i = 2; i * i <= n; i++) {
+		if (primeNumbers[i]) {
+			for (int j = i * i; j <= n; j += i)
+				primeNumbers[j] = false;
+		}
+	}
+}
+
+int solution(string numbers) {
+	int answer = 0;
+	set<int> combinedNumbers;
+	
+	sort(numbers.begin(), numbers.end(), greater<>());
+	eratos(stoi(numbers));
+	
+	do {
+		int number = stoi(numbers);
+		while (number) {
+			combinedNumbers.insert(number);
+			number /= 10;
+		}
+	} while (prev_permutation(numbers.begin(), numbers.end()));
+
+	for (int number : combinedNumbers) {
+		if (primeNumbers[number])
+			answer += 1;
+	}
+
+	return answer;
+}
