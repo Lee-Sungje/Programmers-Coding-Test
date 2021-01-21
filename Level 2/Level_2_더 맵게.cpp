9960580b12f83ba02fed6cc49297463f886bca81
Lee-Sungje <dtw2813@gmail.com>
2021-01-21 23:45:48 +0900

    프로그래머스 코딩테스트 연습 문제 Level2
    ·Level_2_더 맵게 - https://programmers.co.kr/learn/courses/30/lessons/42626

---
+++ Level 2/Level_2_더 맵게.cpp
@@ -0,0 +1,25 @@
+﻿/* 프로그래머스 - Level 2 더 맵게 */
+
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+	int answer = 0;
+	bool is_exceed = false;
+	priority_queue<int, vector<int>, greater<int>> min_heap(scoville.begin(), scoville.end());
+
+	while (!is_exceed && 1 < min_heap.size()) {
+		int first_scoville = min_heap.top(); min_heap.pop();
+		int second_scoville = min_heap.top(); min_heap.pop();
+
+		min_heap.push(first_scoville + (second_scoville * 2));
+		answer += 1;
+
+		if (K <= min_heap.top())
+			is_exceed = true;
+	}
+
+	return is_exceed ? answer : -1;
+}
